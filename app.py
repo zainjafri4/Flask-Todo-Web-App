eaefdd0c66e1010f2d50a040339bb51bc4b1d027
unknown <zainjafri4@gmail.com>
2023-10-09 23:27:56 +0500

    Todo App Intital Commit

---
+++ app.py
@@ -0,0 +1,59 @@
+from typing import Any
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app=Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+   sno=db.Column(db.Integer, primary_key=True)
+   title=db.Column(db.String(200), nullable=False)
+   desc=db.Column(db.String(500), nullable=False)
+   time=db.Column(db.DateTime, default=datetime.utcnow)
+
+   def __reduce__(self) -> str :
+    return f"{self.title} - {self.desc}"
+
+   
+
+@app.route('/', methods = ["GET", "POST"])
+def home():
+    if request.method == "POST":  
+       title=request.form['title']
+       desc=request.form['desc']   
+       todo = Todo(title=title, desc=desc)       
+       db.session.add(todo)
+       db.session.commit()       
+    alltodo=Todo.query.all()
+    return render_template('index.html', alltodo=alltodo) 
+
+@app.route('/delete/<int:sno>')
+def delete(sno):
+  todo=Todo.query.filter_by(sno=sno).first()
+  db.session.delete(todo)
+  db.session.commit()
+  return redirect('/')
+   
+@app.route('/update/<int:sno>', methods = ['GET', 'POST'])
+def update(sno):
+  if request.method == "POST":  
+    title=request.form['title']
+    desc=request.form['desc']   
+    todo=Todo.query.filter_by(sno=sno).first()       
+    todo.title=title
+    todo.desc=desc
+    db.session.add(todo)
+    db.session.commit() 
+    return redirect ('/')   
+  todo=Todo.query.filter_by(sno=sno).first()    
+  return render_template('update.html', todo=todo)
+  
+#   title=request.form['title']
+#   desc=request.form['desc']
+  
+ 
+
+if __name__ == ('__main__'):
+ app.run (debug = True)
